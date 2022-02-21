6e5744ef23dbafdf096bbe374fabad5050d1ff0c
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-21 14:36:23 +0100

    add array object

--- js/app.js
+++ js/app.js
@@ -25,11 +25,32 @@ const text = [
 
 const slideObject = [
     {
-        immagine: '',
-        titolo: '',
-        descrizione: '',
+        immagine: 'img/01.jpg',
+        titolo: 'Svezia',
+        descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        immagine: 'img/02.jpg',
+        titolo: 'Svizzera',
+        descrizione: 'Lorem ipsum',
+    },
+    {
+        immagine: 'img/03.jpg',
+        titolo: 'Gran Bretagna',
+        descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        immagine: 'img/04.jpg',
+        titolo: 'Germania',
+        descrizione: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
+    },
+    {
+        immagine: 'img/05.jpg',
+        titolo: 'Paradise',
+        descrizione: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam',
     },
 ]
+console.log(slideObject);
 
 // recupero dal DOM i contenitori del carusel
 
